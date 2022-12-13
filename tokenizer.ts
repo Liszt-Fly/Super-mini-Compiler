38cb952406f1ad68b13d5398c0fb6ca7d69db66b
Liszt-Fly <1904231450@qq.com>
2022-12-13 16:30:25 +0800

    Part 1 tokenizer

---
+++ tokenizer.ts
@@ -0,0 +1,71 @@
+export enum TokenTypes{
+    Paren,
+    Name,
+    Number
+}
+
+export interface token{
+    type:TokenTypes,
+    value:string
+}
+
+export const tokenizer=(code:string)=>{
+    const tokens:token[]=[]
+    let current=0
+
+    while(current<code.length){
+        let char=code[current]
+
+        const WHITESPACE=/\s/
+        if(WHITESPACE.test(char)){
+            current++
+            continue;
+        }
+        if(char=="("){
+            tokens.push({type:TokenTypes.Paren,value:char})
+            current++
+            continue;
+        }
+        
+
+        if(char==")"){
+     
+            tokens.push({type:TokenTypes.Paren,value:char})
+            current++
+            continue;
+        }
+
+        const LETTERS=/[a-z]/i
+        if(LETTERS.test(char)){
+            let value=""
+            while(LETTERS.test(char)&&current<code.length){
+                value+=char
+                char=code[++current]
+              
+            }
+            tokens.push({
+                type:TokenTypes.Name,
+                value
+            })
+        continue;
+        }
+    
+        const NUMBERS=/[0-9]/
+    
+        if(NUMBERS.test(char)){
+            let value=""
+            while(NUMBERS.test(char)&&current<code.length){
+                value+=char
+                char=code[++current]
+              
+            }
+            tokens.push({
+                type:TokenTypes.Number,
+                value
+            })
+        }
+        continue;
+    }
+  
+    return tokens
+}
